dff9deaf52687965462102be7470cbbbd12e3105
Rajesh <choudharyrajesh729@gmail.com>
2023-03-03 10:20:18 +0530

    error fix

--- script.js
+++ script.js
@@ -17,14 +17,14 @@ const checkWin = ()=>{
         [0, 1, 2,],
         [3, 4, 5,],
         [6, 7, 8,],
-        [0, 3, 6, ],
-        [1, 4, 7, ],
-        [2, 5, 8, ],
-        [0, 4, 8, ],
-        [2, 4, 6, ],
+        [0, 3, 6,],
+        [1, 4, 7,],
+        [2, 5, 8,],
+        [0, 4, 8,],
+        [2, 4, 6,],
     ]
     wins.forEach(e =>{
-        if((boxtext[e[1]].innerText === boxtext[e[2]].innerText) && (boxtext[e[1]].innerText === boxtext[e[2]].innerText) && (boxtext[e[0]].innerText !== "") ){
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[1]].innerText === boxtext[e[2]].innerText) && (boxtext[e[0]].innerText !== "") ){
             document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
             isgameover = true
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
